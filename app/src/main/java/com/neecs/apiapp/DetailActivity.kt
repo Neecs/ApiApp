060f52c9efe2262e6dc143e8a32d816c565de787
Nicolas Gil <nikosgil12@gmail.com>
2024-09-22 18:59:42 -0500

    DetailActivity changed to ViewBinding

--- app/src/main/java/com/neecs/apiapp/DetailActivity.kt
+++ app/src/main/java/com/neecs/apiapp/DetailActivity.kt
@@ -3,26 +3,25 @@ package com.neecs.apiapp
 import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.ImageView
-import android.widget.TextView
 import com.bumptech.glide.Glide
+import com.neecs.apiapp.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailBinding
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val characterName = intent.getStringExtra("characterName")
         val characterImageUrl = intent.getStringExtra("characterImageUrl")
 
-        val characterImageView: ImageView = findViewById(R.id.characterImageView)
-        val characterNameTextView: TextView = findViewById(R.id.characterNameTextView)
-
-        characterNameTextView.text = characterName
+        binding.characterNameTextView.text = characterName
         Glide.with(this)
             .load(characterImageUrl)
-            .into(characterImageView)
+            .into(binding.characterImageView)
     }
 }
